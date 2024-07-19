2f18fe850aa7271042930b9aa2cb206b7be16a40
DmitriyDan <dmzl@mail.ru>
2024-07-19 09:49:43 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = 'Дмитрий'
+print('Name = ' + name)
+age = 38
+print('age = ' + str(age))
+age = age + 2
+print('New Age: '+ str(age))
+is_student = True
+print('Is Student: '+ str(is_student))
