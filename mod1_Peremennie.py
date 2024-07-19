e8868b5c08fbf967dc9f48954089cd35cae7949c
DmitriyDan <dmzl@mail.ru>
2024-07-19 09:07:55 +0300

    Add files via upload

---
+++ mod1_Peremennie.py
@@ -0,0 +1,5 @@
+Kolichestvo_DZ = 12
+Kolichestvo_chasov = 1.5
+Nazvanie_kursa = 'Pyton'
+Vremia_na_odno_zadanie = Kolichestvo_chasov / Kolichestvo_DZ
+print('Курс: Python, всего задач:'+ str(Kolichestvo_DZ)+', затрачено часов:'+str(Kolichestvo_chasov)+', среднее время выполнения:' + str(Vremia_na_odno_zadanie))
