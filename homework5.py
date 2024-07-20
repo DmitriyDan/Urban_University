768866f322dcfee8624d0b301788f41cf9a286d6
DmitriyDan <dmzl@mail.ru>
2024-07-20 08:32:14 +0300

    Add files via upload

--- homework5.py
+++ homework5.py
@@ -1,5 +1,8 @@
 immutable_var = 'odin', 2, 'tri', 4, True
 print('Кортеж: ', immutable_var)
+# immutable_var[0] = 1
+# Кортеж относится  к неизменяемым типам данных. Поэтому изменить элемент
+# кортежа на другое значение не получиться.
 mutable_list = ['odin', 2, 'tri', 4, True]
 print('Список: ',mutable_list)
 mutable_list[0] = 1
