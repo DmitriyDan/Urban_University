0743521c4d664978c07ffa5d61c5251133194633
DmitriyDan <dmzl@mail.ru>
2024-08-22 11:08:13 +0300

    Add files via upload

--- module_5_3.py
+++ module_5_3.py
@@ -20,13 +20,13 @@ class House:
 
     def __eq__(self, other):
         return self.number_of_floors == other.number_of_floors
-    #
-    # def __lt__(self, other):
-    #     return self.number_of_floors < other.number_of_floors
-    #
-    # def __le__(self, other):
-    #     return self.number_of_floors <= other.number_of_floors
-    #
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
     def __gt__(self, other):
         return self.number_of_floors > other.number_of_floors
 
